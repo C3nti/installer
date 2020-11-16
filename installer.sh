e43e6d200949e845e8f2faa82de2c63214087532
C3nti <68709331+C3nti@users.noreply.github.com>
2020-11-16 08:47:35 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,3 @@
   
 #!/bin/bash
-sudo add-apt-repository multiverse \
-&& sudo apt update \ 
-&& sudo apt upgrade \
-&& sudo apt install git spotify-client steam-installer
+sudo add-apt-repository multiverse && sudo apt update && sudo apt upgrade && sudo apt install git spotify-client steam-installer
