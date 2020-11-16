a699f90f71bb3dc79bef6e115df2b194e8a01f06
C3nti <68709331+C3nti@users.noreply.github.com>
2020-11-16 09:10:00 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,9 +4,9 @@ sudo add-apt-repository -y multiverse
 sudo apt update && sudo apt upgrade
 sudo apt install steam-installer -y
 sudo apt install python3-pip -y
-sudo snap install spotify -y
-sudo snap install code -y
-sudo snap install powershell -y
-sudo snap install fast -y
+sudo snap install spotify
+sudo snap install code
+sudo snap install powershell
+sudo snap install fast
 alias pip=pip3
 rm -rf ~/installer
