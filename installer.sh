80f1aae7eb3b3d66c266cacc053f7427188f133c
C3nti <68709331+C3nti@users.noreply.github.com>
2020-11-16 09:15:32 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -9,4 +9,7 @@ sudo snap install spotify
 sudo snap install code --classic
 sudo snap install powershell --classic
 alias pip=pip3
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
 rm -rf ~/installer
