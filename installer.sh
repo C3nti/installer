9f43efd94380d1302e402fe1b131c00c099811ad
C3nti <68709331+C3nti@users.noreply.github.com>
2020-11-17 18:39:38 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,9 +1,8 @@
   
 #!/bin/bash
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" | sudo tee /etc/apt/sources.list.d/teams.list
 sudo add-apt-repository -y multiverse
 sudo apt update && sudo apt upgrade
+sudo apt install curl
 sudo apt install steam-installer -y
 sudo apt install python3-pip -y
 sudo apt install speedtest-cli -y
@@ -11,6 +10,8 @@ sudo apt install teams
 sudo snap install spotify
 sudo snap install code --classic
 sudo snap install powershell --classic
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" | sudo tee /etc/apt/sources.list.d/teams.list
 alias pip=pip3
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
