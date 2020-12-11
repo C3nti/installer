baf88d1dc00e45454e68c6e36b811c9d890f18e1
C3nti <68709331+C3nti@users.noreply.github.com>
2020-12-11 12:08:27 +0100

    Update installer.sh
    removed Gnome settings and none working teams installation

--- installer.sh
+++ installer.sh
@@ -27,15 +27,15 @@ sudo snap install gmail-desktop
 sudo snap install skype --classic
 sudo snap install code --classic
 sudo snap install powershell --classic
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" | sudo tee /etc/apt/sources.list.d/teams.list
-sudo apt install teams
+#curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+#echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" | sudo tee /etc/apt/sources.list.d/teams.list
+#sudo apt install teams
 alias pip=pip3
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
 cp ~/installer/background.jpg ~/Pictures/
-gsettings set org.gnome.desktop.background picture-uri file:////home/wmmaper/Pictures/background.jpg
-gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+#gsettings set org.gnome.desktop.background picture-uri file:////home/wmmaper/Pictures/background.jpg
+#gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 rm -rf ~/installer
 sudo apt autoremove -y
